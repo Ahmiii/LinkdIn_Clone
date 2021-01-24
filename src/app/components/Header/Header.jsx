e20b3276a15ca3a5a6ca6bf91f331aa12849ed37
Ahmiii <ahmii.shaikh18@gmail.com>
2021-01-24 23:30:47 +0500

    working on header

---
+++ src/app/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header_Left from "./header_left/header_left";
+import Header_Right from "./header_right/header_right";
+const Header = () => {
+  return (
+    <>
+      <Header_Right />
+      <Header_Left />
+    </>
+  );
+};
+
+export default Header;
---
+++ src/app/components/Header/header_left/header_left.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const header_left = () => {
+  return <div></div>;
+};
+
+export default header_left;
