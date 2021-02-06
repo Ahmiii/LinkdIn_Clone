ac66168df43d1dcac892df2564abd521e0974164
Ahmiii <ahmii.shaikh18@gmail.com>
2021-02-06 21:24:26 +0500

    header completed

--- src/app/components/Header/header_right/headerOptions/headerOptions.jsx
+++ src/app/components/Header/header_right/headerOptions/headerOptions.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./headerOptions.css";
-const headerOptions = ({ Icon, title }) => {
+import { Avatar } from "@material-ui/core";
+const headerOptions = ({ avatar, Icon, title }) => {
   console.log("heelo world");
   return (
     <div className="headerOption">
       {Icon && <Icon className="headerOption__icon" />}
+      {avatar && <Avatar className="headerOption__icon" src={avatar} />}
       <h3 className="headerOption__title">{title}</h3>
     </div>
   );
--- src/app/components/Header/header_right/header_right.jsx
+++ src/app/components/Header/header_right/header_right.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import HeaderOption from "./headerOptions/headerOptions";
 import HomeIcon from "@material-ui/icons/Home";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BuisnessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationIcon from "@material-ui/icons/Chat";
 import "./header_right.css";
 
 const header_right = () => {
@@ -11,6 +14,13 @@ const header_right = () => {
     <div className="header__right">
       <HeaderOption Icon={HomeIcon} title="Home" />
       <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+      <HeaderOption Icon={BuisnessCenterIcon} title="Jobs" />
+      <HeaderOption Icon={ChatIcon} title="Messages" />
+      <HeaderOption Icon={NotificationIcon} title="Notification" />
+      <HeaderOption
+        avatar="https://media-exp1.licdn.com/dms/image/C5103AQH5Rq4kt7yCxQ/profile-displayphoto-shrink_800_800/0/1577266457385?e=1617840000&v=beta&t=AOPYXG2HGgctT6r-ffvfbVp3Mb1poY9SRBshLLO4LhA"
+        title="Ahmed Faraz"
+      />
     </div>
   );
 };
